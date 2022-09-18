de3e03844e25eac5d23a7ce10689742e1ac14adc
guilhemeicarofr <guilhemeicarofr@gmail.com>
2022-09-18 14:29:30 -0300

    Number of itens on cart notification fixed

--- src/Components/Pages/Cart/CartMenu.js
+++ src/Components/Pages/Cart/CartMenu.js
@@ -1,6 +1,12 @@
+import styled from 'styled-components';
+
 export default function CartMenu() {
     return (
-        <>
-        </>
+        <CartMenuContainer>
+        </CartMenuContainer>
     );
 }
+
+const CartMenuContainer = styled.div`
+    width: 100%
+`;
